5377cbc58b48bbdcbf32d187551173c97c4ec593
Sharmaine Requerme <srqrme@gmail.com>
2018-08-18 22:38:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,14 +12,14 @@ class Triangle
   def kind
     if @length1 <= 0 || @length2 <= 0 || length3 <= 0
       raise TriangleError
-    elsif @length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1
+    elsif @length1 + @length2 <= @length3 || @length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
       raise TriangleError
     else
       if length1 == length2 && length2 == length3
         :equilateral
       elsif length2 == length3 || length1 == length3 || length1 == length2
         :isosceles
-      else
+      elsif @length1 != @length2 && @length2 != @length3 && @length1 != @length3
         :scalene
       end
     end
